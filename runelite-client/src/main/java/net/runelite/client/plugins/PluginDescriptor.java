da88cd5ba6bc9c0cf4d30c2ec2605a0f44f77d07
MakeCD <217837819+MakeCD@users.noreply.github.com>
2025-06-25 03:27:58 +0300

    feat(wintertodt): add initial plugin implementation

--- runelite-client/src/main/java/net/runelite/client/plugins/PluginDescriptor.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/PluginDescriptor.java
@@ -64,6 +64,9 @@ public @interface PluginDescriptor
   	String Cardew = "<html>[<font color=#824BA3>CD</font>]";
 	String Bolado = "<html>[<font color=#FF0000><b>\uD83D\uDE21</b></font>] ";
  	String Choken = "<html>[<font color=#8A2BE2>\u03A9</font>] ";
+	String MKE = "<html>[<font color=#8A2BE2>MKE</font>] ";
+
+
 
 	String name();
 
